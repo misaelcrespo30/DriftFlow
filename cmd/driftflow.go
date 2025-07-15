4bf0f26f110c35b024944b2f5f95ea2dbf69a433
Misael Crespo <misaelcrespo30@gmail.com>
2025-07-15 17:50:37 -0400

    optimitation

--- cmd/driftflow.go
+++ cmd/driftflow.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/misaelcrespo30/DriftFlow/internal/models"
-	"github.com/misaelcrespo30/DriftFlow/state"
-	"os"
-
 	"github.com/misaelcrespo30/DriftFlow/cli"
+	"os"
 )
 
 func main() {
-	state.SetModels(models.Models())
+
 	if err := cli.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
--- internal/models/matter_related.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type MatterRelated struct {
-	gorm.Model
-	MatterID      uint `gorm:"column:matter_id" json:"matter_id"`
-	ActivityLogID uint `gorm:"column:activity_log_id" json:"activity_log_id"`
-}
